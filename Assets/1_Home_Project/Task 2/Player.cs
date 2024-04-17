eabb39dd96e26c8ee22315840a227c3ed4dc33c7
Stanislav N. <stanislav_developer@mail.ru>
2024-04-17 15:25:39 +0500

    add task 1

---
+++ Assets/1_Home_Project/Task 2/Player.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets
+{
+    public class Player : MonoBehaviour
+    {
+        private IWeapon weapon;
+
+
+    }
+}
---
+++ Assets/1_Home_Project/Task 2/Weapon.cs
@@ -0,0 +1,12 @@
+using Assets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapon : IWeapon
+{
+    public void Shoot()
+    {
+        throw new System.NotImplementedException();
+    }
+}
